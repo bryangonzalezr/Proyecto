902aba330eb2a37c21226ff07b3928fce07eb57a
Bryan Gonzalez <bryan.gonzalezr@usm.cl>
2021-01-12 13:26:21 -0300

    changes

--- IWGProyecto/web-desktop/src/settings.aa07f.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default","circle"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Menu.fire",orientation:"",jsList:[],bundleVers:{internal:"279e0",main:"c15e8"}};
